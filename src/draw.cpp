c126caf8117d20161a695d2c1350713841837f37
Angélique <angeliquelebas2008@gmail.com>
2021-06-26 22:03:28 +0200

    création d'une deuxième pièce
    ok

--- src/draw.cpp
+++ src/draw.cpp
@@ -15,36 +15,58 @@ void drawScene(Camera camera){
 
 	glDrawRepere(100.0);
 
+	/* gestion de l'affichage (on ne veut pas voir derrière un objet) */
+	glEnable(GL_DEPTH_TEST);
+    glEnable(GL_TEXTURE_2D);
+
+	/* PREMIERE PIECE - SEJOUR */
+
 	/* dessin du sol */
 	glColor3f(0.9 , 0.9, 1);
-	//glDrawQuad(1, 1, 1, 50, 50, 0.5);
-	glDrawQuad(0.5, 0.5, 0, 50, 50, 0.5);
+	glDrawQuad(25, 25, 0.5, 50, 50, 1);
 
 	/* dessin des murs sans porte */
 
-	//cuisine-entrée
-	glColor3f(0.8 , 0.8, 1);
-	//glDrawQuad(0.5, 1, 1, 0.5, 50, 20);
-	glDrawQuad(0.25, 0.5, 0.5, 0.5, 50, 20);  
-	//fenetres-sejour
+	//fenetres
 	glColor3f(0.7 , 0.7, 1);
-	//glDrawQuad(1, 0.5, 1, 50, 0.5, 20);
-	glDrawQuad(0.5, 1, 0.5, 50, 0.5, 20); 
-	//sejour-sdb
+	glDrawQuad(25, 0.5, 10, 50, 1, 20); 
+	//sdb
 	glColor3f(0.6 , 0.6, 1);
-	//glDrawQuad(200, 1, 1, 0.5, 50, 20);
-	glDrawQuad(100, 0.5, 0.5, 0.5, 50, 20);
+	glDrawQuad(50, 25, 10, 1, 50, 20);
 
 	/* mur avec porte */
 
-	//entrée principale
+	//porte séjour-cuisine
+	glColor3f(0.8 , 0.8, 1);
+	glDrawQuad(0.5, 15, 10, 1, 30, 20); 
+	glDrawQuad(0.5, 45, 10, 1, 10, 20);
+	glDrawQuad(0.5, 35, 18, 1, 10, 4);
+	//porte séjour-entrée
 	glColor3f(0.5 , 0.5, 1);
-	//glDrawQuad(200, 1, 1, 30, 0.5, 20);
-	//glDrawQuad(1, 200, 1, 10, 0.5, 20);
-	glDrawQuad(0.5, 100, 0.5, 10, 0.5, 20);
-	glDrawQuad(1.16, 100, 0.5, 30, 0.5, 20);
-	glDrawQuad(1.5, 100, 4.5, 10, 0.5, 4);
-	
+	glDrawQuad(5, 50, 10, 10, 1, 20);
+	glDrawQuad(35, 50, 10, 30, 1, 20);
+	glDrawQuad(15, 50, 18, 10, 1, 4);
+
+	/* DEUXIEME PIECE - CUISINE */
+
+	/* dessin du sol */
+	glColor3f(1 , 0.9, 0.9);
+	glDrawQuad(-10, 25, 0.5, 20, 50, 1);
+
+	/* dessin des murs sans porte */
+
+	//fenetre
+	glColor3f(1 , 0.8, 0.8);
+	glDrawQuad(-10, 0, 10, 20, 1, 20); 
+	//extérieur droit
+	glColor3f(1 , 0.7, 0.7);
+	glDrawQuad(-20, 25, 10, 1, 50, 20);
+	//entrée
+	glColor3f(1 , 0.6, 0.6);
+	glDrawQuad(-10, 50, 10, 20, 1, 20);
+
+	/* TROISIEME PIECE - SALLE DE BAIN */
+
 
 	/* Fin du dessin */
 	glPopMatrix();
@@ -73,8 +95,7 @@ void glDrawRepere(float length){
 
 void glDrawQuad(float x, float y, float z, float largeur, float longueur, float hauteur){
 	glPushMatrix();
-	glTranslated(largeur*x, longueur*y, hauteur*z);
-	//glTranslated(x, y, z);
+	glTranslated(x, y, z);
 	glScaled(largeur, longueur, hauteur);
 	glutSolidCube(1);
 	glPopMatrix();
